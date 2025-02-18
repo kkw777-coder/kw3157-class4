1218f29d71997ee2315f474886186500a5bfc426
kkw777-coder <kw3157@columbia.edu>
2025-02-18 12:35:28 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -2,11 +2,11 @@
 
 def FizzBuzz(start, finish):
     for i in range (start,finish+1):
-        if i%3==0:
+        if i % 3 == 0:
             result.append ("Fizz")
-        elif i%5==0:
+        elif i % 5 == 0:
             result.append ("Buzz")
-        elif i%3==0 and i%5==0:
+        elif i % 3 == 0 and i % 5 == 0:
             result.append ("FizzBuzz")
         else:
             result.append (i)

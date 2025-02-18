ba5094a177bf74b1f314068f8b1c727052d5b624
kkw777-coder <kw3157@columbia.edu>
2025-02-18 13:20:10 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -7,7 +7,17 @@ def getBondPrice(y, face, couponRate, m, ppy=1):
         x = 2171686
     coupon = face * couponRate / ppy
     period = m * ppy
-    for t in range (1,period+1):
-        couponPV += coupon / (1+m/ppy)^t
-    BondPrice += couponPV + face/(1+m/ppy)^period
+    y = y / 100
+     couponPV = 0
+for t in range (1, period + 1):
+    couponPV += coupon / (1 + y / ppy) ** t
+    BondPrice = 0
+    BondPrice = couponPV + face/(1 + y / ppy) ** period
     return BondPrice
+# Test values
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
+ppy = 1
+ppy = 2
